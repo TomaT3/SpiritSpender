79e645e5d1579556c0fdfce1177435bf138fc771
Michael Neu <m.neu.1983@live.com>
2020-02-25 23:08:44 +0100

    add mongoDb BSON-serializers for UnitsNet structs

---
+++ SpiritSpenderServer/SpiritSpenderServer/Persistence/Serialization/UnitNetSerializationProvider.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson.Serialization;
+using System;
+using UnitsNet;
+
+namespace SpiritSpenderServer.Persistence.Serialization
+{
+    public class UnitNetSerializationProvider : IBsonSerializationProvider
+    {
+        public IBsonSerializer GetSerializer(Type type)
+            => type switch
+            {
+                Type _ when type == typeof(Length) => new LengthBsonSerializer(),
+                Type _ when type == typeof(Speed) => new SpeedBsonSerializer(),
+                Type _ when type == typeof(Acceleration) => new AccelerationBsonSerializer(),
+                Type _ when type == typeof(Duration) => new DurationBsonSerializer(),
+                _ => null,
+	        };
+    }
+}
