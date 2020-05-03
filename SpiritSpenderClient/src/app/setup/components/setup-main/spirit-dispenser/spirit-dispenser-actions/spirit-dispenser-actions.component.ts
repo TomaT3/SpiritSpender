41096172498bddfd7e6d8e75db29f27f2e9d39e2
Niklas <niklas@nokxs.de>
2020-05-03 20:41:36 +0200

    Add bottle change mode

--- SpiritSpenderClient/src/app/setup/components/setup-main/spirit-dispenser/spirit-dispenser-actions/spirit-dispenser-actions.component.ts
+++ SpiritSpenderClient/src/app/setup/components/setup-main/spirit-dispenser/spirit-dispenser-actions/spirit-dispenser-actions.component.ts
@@ -3,25 +3,34 @@ import { SpiritDispenserApiService } from 'src/app/setup/services/spirit-dispens
 
 @Component({
   selector: 'app-spirit-dispenser-actions',
+  styleUrls: ['./spirit-dispenser-actions.component.scss'],
   templateUrl: './spirit-dispenser-actions.component.html',
-  styleUrls: ['./spirit-dispenser-actions.component.scss']
 })
-export class SpiritDispenserActionsComponent implements OnInit {
+export class SpiritDispenserActionsComponent {
+
+  public isBottleChangeModeActive = false;
 
   constructor(private spiritDispenserApi: SpiritDispenserApiService) { }
 
-  ngOnInit(): void {
+  public async toggleBottleChangeMode(): Promise<void> {
+    if (this.isBottleChangeModeActive) {
+      await this.spiritDispenserApi.closeSpiritSpender();
+    } else {
+      await this.spiritDispenserApi.openSpiritSpender();
+    }
+
+    this.isBottleChangeModeActive = !this.isBottleChangeModeActive;
   }
 
-  public async closeSpiritSpender() : Promise<void> {
+  public async closeSpiritSpender(): Promise<void> {
     await this.spiritDispenserApi.closeSpiritSpender();
   }
 
-  public async openSpiritSpender() : Promise<void> {
+  public async openSpiritSpender(): Promise<void> {
     await this.spiritDispenserApi.openSpiritSpender();
   }
 
-  public async fillGlas() : Promise<void> {
+  public async fillGlas(): Promise<void> {
     await this.spiritDispenserApi.fillGlas();
   }
 }
