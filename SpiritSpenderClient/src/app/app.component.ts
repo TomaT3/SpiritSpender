da29301d57db1a0c22f5c5204a16740073326898
Niklas <niklas@nokxs.de>
2020-06-10 22:05:38 +0200

    add signalr to client

--- SpiritSpenderClient/src/app/app.component.ts
+++ SpiritSpenderClient/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from "@angular/platform-browser";
+import { HubConnection, HubConnectionBuilder } from '@aspnet/signalr';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +11,11 @@ import { DomSanitizer } from "@angular/platform-browser";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  private hubConnection: HubConnection;
+
   title = 'SpiritSpenderClient';
   navLinks: any[];
-  activeLinkIndex = -1; 
+  activeLinkIndex = -1;
   constructor(
     private router: Router,
     private matIconRegistry: MatIconRegistry,
@@ -34,6 +38,15 @@ export class AppComponent {
     this.router.events.subscribe((res) => {
         this.activeLinkIndex = this.navLinks.indexOf(this.navLinks.find(tab => tab.path === '.' + this.router.url));
     });
+
+    // todo: This should be in its own service
+    this.hubConnection = new HubConnectionBuilder().withUrl(environment.baseUrl).build();
+    this.hubConnection
+      .start()
+      .then(() => console.log("SignalR connection established"))
+      .catch(err => {
+        console.error(`Error while establishing SignalR conneciton: ${err}`);
+      });
   }
 
   private registerIcons(): void {
