e56c22a20228102ac2c9563ecb6e3697d6cf96c4
Michael Neu <m.neu.1983@live.com>
2021-05-23 13:01:14 +0200

    add try reconnect to signalR in client

--- SpiritSpenderClient/src/app/shared/services/signal-r/axis-signal-r.service.ts
+++ SpiritSpenderClient/src/app/shared/services/signal-r/axis-signal-r.service.ts
@@ -18,35 +18,44 @@ export class AxisSignalRService {
 
 
   constructor() {
-    this.createConnection();  
-    this.registerOnServerEvents();  
-    this.startConnection();  
+    this.createConnection();
+    this.registerOnServerEvents();
+    this.startConnection();
+  }
+
+  private createConnection() {
+    this._hubConnection = new HubConnectionBuilder()
+      .withUrl(API_URL + 'axis')
+      .build();
+
+      this._hubConnection.onclose((error => {
+        console.log('Hub connection closed: ' + error);
+        this.tryReconnect();
+      }))
   }  
-  
-  private createConnection() {  
-    this._hubConnection = new HubConnectionBuilder()  
-      .withUrl(API_URL + 'axis')  
-      .build();  
-  }  
-  
-  private startConnection(): void {  
-    this._hubConnection  
+
+  private startConnection(): void {
+    this._hubConnection
       .start()
-      .then(() => {  
-        this.connectionIsEstablished = true;  
-        console.log('Hub connection started');  
-        this.connectionEstablished.emit(true);  
-      })  
-      .catch(err => {  
-        console.log('Error while establishing connection, retrying...');  
+      .then(() => {
+        this.connectionIsEstablished = true;
+        console.log('Hub connection started');
+        this.connectionEstablished.emit(true);
+      })
+      .catch(err => {
+        console.log('Error while establishing hub connection, retrying...');
         this.connectionIsEstablished = false;
-        setTimeout(function () { this.startConnection(); }, 5000);  
-      });  
-  }  
-  
-  private registerOnServerEvents(): void {  
-    this._hubConnection.on('PositionChanged', (data: PositionDto) => {  
-      this.positionChanged.emit(data);  
-    });  
-  }  
+        this.tryReconnect();
+      });
+  }
+
+  private tryReconnect(): void {
+    setTimeout(() => { this.startConnection(); }, 5000);
+  }
+
+  private registerOnServerEvents(): void {
+    this._hubConnection.on('PositionChanged', (data: PositionDto) => {
+      this.positionChanged.emit(data);
+    });
+  }
 }
