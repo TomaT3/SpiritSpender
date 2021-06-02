a76aaf29465506821243927e24aa3410cbf65629
Michael Neu <m.neu.1983@live.com>
2021-06-02 17:00:47 +0200

    add iobroker connection

--- SpiritSpenderServer/src/SpiritSpenderServer/Config/ServerConfig.cs
+++ SpiritSpenderServer/src/SpiritSpenderServer/Config/ServerConfig.cs
@@ -3,5 +3,6 @@
     public class ServerConfig
     {
         public MongoDBConfig MongoDB { get; set; } = new MongoDBConfig();
+        public IoBrokerConfig IoBroker { get; set; } = new IoBrokerConfig();
     }
 }
