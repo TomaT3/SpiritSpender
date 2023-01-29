ecec7beac34b7857adb38ecde48c00ab2e7a2042
TomaT3 <m.neu@gmx.de>
2023-01-29 17:49:46 +0100

    add serial communictation class

---
+++ SpiritSpenderServer/src/SpiritSpenderServer/NC_Communication/SerialCommunication.cs
@@ -0,0 +1,55 @@
+﻿namespace SpiritSpenderServer.NC_Communication;
+
+using System;
+using System.IO.Ports;
+
+public class SerialCommunication
+{
+    private SerialPort _serialPort;
+
+    public SerialCommunication()
+    {
+        //var port = @"/dev/ttyS0";
+        var port = @"/dev/ttyAMA0";
+        var baudrate = 115200;
+        _serialPort = new SerialPort(port, baudrate);
+        _serialPort.ReadTimeout = System.IO.Ports.SerialPort.InfiniteTimeout;
+        _serialPort.WriteTimeout = 1500;
+
+    }
+
+    public void OpenPort()
+    {
+        _serialPort.Open();
+    }
+
+    public void StartListenToSerialPort()
+    {
+        while (true)
+        {
+            try
+            {
+                string message = _serialPort.ReadLine();
+                Console.WriteLine($"Received: {message}");
+            }
+            catch (TimeoutException timeoutException)
+            {
+                //Console.WriteLine(timeoutException);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+
+    public void Write(string text)
+    {
+        try
+        {
+            _serialPort.Write(text);
+            Console.WriteLine($"Write:  {text}");
+        }
+        catch (TimeoutException) { }
+    }
+}
