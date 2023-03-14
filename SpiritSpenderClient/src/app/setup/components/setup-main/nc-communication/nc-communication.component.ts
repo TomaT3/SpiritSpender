420e877f40eea6c9f4c2eb2a602cad48ebbb0ce1
TomaT3 <m.neu@gmx.de>
2023-03-14 23:33:44 +0100

    add nc communication component

--- SpiritSpenderClient/src/app/setup/components/setup-main/nc-communication/nc-communication.component.ts
+++ SpiritSpenderClient/src/app/setup/components/setup-main/nc-communication/nc-communication.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NcCommunicationComponent implements OnInit {
 
+  value: string = '';
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public sendNcCommand(): void {
+
   }
 
 }
